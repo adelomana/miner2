9d41aaede1152c8e9cf866b9d940f95ce077ee32
Adrian de Lomana <alomana@systemsbiology.org>
2019-05-24 14:51:58 -0700

    minor update on mechanistic inference

--- miner2/mechanisticInference.py
+++ miner2/mechanisticInference.py
@@ -1,7 +1,57 @@
-import datetime,pandas,numpy,scipy,os,pickle
-import sklearn,sklearn.decomposition,sys
+import datetime,pandas,numpy,os,pickle,sys
+import sklearn,sklearn.decomposition
+import scipy,scipy.stats
 from pkg_resources import Requirement, resource_filename
 import miner2.coexpression
+
+### to remove
+def splitForMultiprocessing(vector,cores):
+    
+    partition = int(len(vector)/cores)
+    remainder = len(vector) - cores*partition
+    starts = np.arange(0,len(vector),partition)[0:cores]
+    for i in range(remainder):
+        starts[cores-remainder+i] = starts[cores-remainder+i] + i    
+
+    stops = starts+partition
+    for i in range(remainder):
+        stops[cores-remainder+i] = stops[cores-remainder+i] + 1
+        
+    return zip(starts,stops)
+
+def splitForMultiprocessing(vector,cores):
+    
+    partition = int(len(vector)/cores)
+    remainder = len(vector) - cores*partition
+    starts = numpy.arange(0,len(vector),partition)[0:cores]
+    for i in range(remainder):
+        starts[cores-remainder+i] = starts[cores-remainder+i] + i    
+
+    stops = starts+partition
+    for i in range(remainder):
+        stops[cores-remainder+i] = stops[cores-remainder+i] + 1
+        
+    return zip(starts,stops)
+
+def multiprocess(function,tasks):
+    import multiprocessing, multiprocessing.pool
+    hydra=multiprocessing.pool.Pool(len(tasks))  
+    output=hydra.map(function,tasks)   
+    hydra.close()
+    hydra.join()
+    return output
+
+def condenseOutput(output):
+    
+    results = {}
+    for i in range(len(output)):
+        resultsDict = output[i]
+        keys = resultsDict.keys()
+        for j in range(len(resultsDict)):
+            key = keys[j]
+            results[key] = resultsDict[key]
+    return results
+### to remove
 
 def axisTfs(axesDf,tfList,expressionData,correlationThreshold=0.3):
     
@@ -21,44 +71,16 @@ def axisTfs(axesDf,tfList,expressionData,correlationThreshold=0.3):
             tfDict[axes[axis]] = tfs
 
         return tfDict
-
-    print('len(axesArray.shape[0])',axesArray.shape[0])
     
     for axis in range(axesArray.shape[0]):
-        
-        
         tfCorrelation = miner2.coexpression.pearson_array(tfArray,axesArray[axis,:])
-        #print('axis,correlationThreshold',axis,correlationThreshold,tfCorrelation.shape)
-        a=numpy.abs(tfCorrelation)
-        b=correlationThreshold
-        print('a,b',type(a),a.shape,b)
-
-        c=numpy.where(numpy.abs(tfCorrelation) >= correlationThreshold)[0]
-        print('c',c,c.shape)
-
-        alternative=numpy.where(numpy.isnan(tfCorrelation))
-        alternative2=numpy.where(alternative[0] == False and numpy.abs(tfCorrelation) >= correlationThreshold)
-        print('alternative2',alternative2)
-        sys.exit()
-        
-        tfDict[axes[axis]] = tfs[numpy.where(numpy.abs(tfCorrelation) >= correlationThreshold)[0]]
-    print('completed')
+        ### ALO, fixed warning over nan evaluations
+        condition1=numpy.greater_equal(numpy.abs(tfCorrelation),correlationThreshold,where=numpy.isnan(tfCorrelation) == False)
+        condition2=numpy.isnan(tfCorrelation)
+        tfDict[axes[axis]]=tfs[numpy.where(numpy.bitwise_and(condition1 == True, condition2 == False))[0]]
+        ### end ALO
     
     return tfDict
-
-def splitForMultiprocessing(vector,cores):
-    
-    partition = int(len(vector)/cores)
-    remainder = len(vector) - cores*partition
-    starts = numpy.arange(0,len(vector),partition)[0:cores]
-    for i in range(remainder):
-        starts[cores-remainder+i] = starts[cores-remainder+i] + i    
-
-    stops = starts+partition
-    for i in range(remainder):
-        stops[cores-remainder+i] = stops[cores-remainder+i] + 1
-        
-    return zip(starts,stops)
 
 def enrichment(axes,revisedClusters,expressionData,correlationThreshold=0.3,numCores=1,p=0.05,database="tfbsdb_tf_to_genes.pkl"):
     
@@ -76,15 +98,33 @@ def enrichment(axes,revisedClusters,expressionData,correlationThreshold=0.3,numC
     tfs = list(tfToGenes.keys())
     tfMap = axisTfs(axes,tfs,expressionData,correlationThreshold=correlationThreshold)
 
-    taskSplit = splitForMultiprocessing(revisedClusters.keys(),numCores)
-    print('taskSplit',taskSplit)
+    tasks=[[clusterKey,(allGenes,revisedClusters,tfMap,tfToGenes,p)] for clusterKey in list(revisedClusters.keys())]
+    hydra=multiprocessing.pool.Pool(numCores)
+    results=hydra.map(tfbsdbEnrichment,tasks)
+
+    mechanisticOutput={}
+    for result in results:
+        for key in result.keys():
+            if key not in mechanisticOutput:
+                mechanisticOutput[key]=result[key]
+            else:
+                print('key twice')
+                sys.exit()
+    print('completed')
+
     sys.exit()
-    
-    tasks = [[taskSplit[i],(allGenes,revisedClusters,tfMap,tfToGenes,p)] for i in range(len(taskSplit))]
-    tfbsdbOutput = multiprocess(tfbsdbEnrichment,tasks)
-    mechanisticOutput = condenseOutput(tfbsdbOutput)
+
         
     return mechanisticOutput
+
+def hyper(population,set1,set2,overlap):
+    
+    b = max(set1,set2)
+    c = min(set1,set2)
+    hyp = scipy.stats.hypergeom(population,b,c)
+    prb = sum([hyp.pmf(l) for l in range(overlap,c+1)])
+    
+    return prb 
 
 def principalDf(dict_,expressionData,regulons=None,subkey='genes',minNumberGenes=8,random_state=12):
 
@@ -122,3 +162,46 @@ def principalDf(dict_,expressionData,regulons=None,subkey='genes',minNumberGenes
     principalMatrix = pandas.concat(pcDfs,axis=1)
         
     return principalMatrix
+
+def tfbsdbEnrichment(task):
+    
+    start, stop = task[0]
+    allGenes,revisedClusters,tfMap,tfToGenes,p = task[1]
+    keys = revisedClusters.keys()[start:stop]
+
+    if len(allGenes) == 1:
+    
+        population_size = int(allGenes[0])
+        clusterTfs = {}
+        for key in keys:
+            for tf in tfMap[str(key)]:    
+                hits0TfTargets = tfToGenes[tf]  
+                hits0clusterGenes = revisedClusters[key]
+                overlapCluster = list(set(hits0TfTargets)&set(hits0clusterGenes))
+                if len(overlapCluster) <= 1:
+                    continue
+                pHyper = hyper(population_size,len(hits0TfTargets),len(hits0clusterGenes),len(overlapCluster))
+                if pHyper < p:
+                    if key not in clusterTfs.keys():
+                        clusterTfs[key] = {}
+                    clusterTfs[key][tf] = [pHyper,overlapCluster]
+
+                            
+    elif len(allGenes) > 1:
+        
+        population_size = len(allGenes)
+        clusterTfs = {}
+        for key in keys:
+            for tf in tfMap[str(key)]:    
+                hits0TfTargets = list(set(tfToGenes[tf])&set(allGenes))   
+                hits0clusterGenes = revisedClusters[key]
+                overlapCluster = list(set(hits0TfTargets)&set(hits0clusterGenes))
+                if len(overlapCluster) <= 1:
+                    continue                
+                pHyper = hyper(population_size,len(hits0TfTargets),len(hits0clusterGenes),len(overlapCluster))
+                if pHyper < p:
+                    if key not in clusterTfs.keys():
+                        clusterTfs[key] = {}
+                    clusterTfs[key][tf] = [pHyper,overlapCluster]
+
+    return clusterTfs
