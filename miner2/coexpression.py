d1e30d242de7bf9442e2a0651f1551852d0df87f
Wei-ju Wu <weiju.wu@gmail.com>
2019-05-29 13:15:42 -0700

    refactoring: naming convention

--- miner2/coexpression.py
+++ miner2/coexpression.py
@@ -3,71 +3,78 @@ import sklearn,sklearn.decomposition
 import multiprocessing, multiprocessing.pool
 from collections import Counter
 
-def cluster(expressionData,minNumberGenes=6,
-            minNumberOverExpSamples=4,
-            maxSamplesExcluded=0.50,
+def cluster(expression_data, min_number_genes=6,
+            min_number_overexp_samples=4,
+            max_samples_excluded=0.50,
             random_state=12,
-            overExpressionThreshold=80, numCores=1):
+            overexpression_threshold=80,
+            num_cores=1):
+    """
+    Create a list of initial clusters. This is a list of list of gene names
+    """
 
     print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S \t coexpression"))
 
-    df = expressionData.copy()
+    df = expression_data.copy()
 
-    maxStep = int(numpy.round(10*maxSamplesExcluded))
-    allGenesMapped = []
-    bestHits = []
+    max_step = int(numpy.round(10 * max_samples_excluded))
+    all_genes_mapped = []
+    best_hits = []
 
-    zero = numpy.percentile(expressionData,0)
-    expressionThreshold = numpy.mean([numpy.percentile(expressionData.iloc[:,i][expressionData.iloc[:,i]>zero],overExpressionThreshold) for i in range(expressionData.shape[1])])
+    zero = numpy.percentile(expression_data, 0)
+    expression_threshold = numpy.mean([numpy.percentile(expression_data.iloc[:,i][expression_data.iloc[:,i] > zero], overexpression_threshold) for i in range(expression_data.shape[1])])
 
     trial = -1
 
-    for step in range(maxStep):
-        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S \t working on coexpression step {} out of {}".format(step+1,maxStep)))
-        trial+=1
-        genesMapped = []
-        bestMapped = []
+    for step in range(max_step):
+        print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S \t working on coexpression step {} out of {}".format(step + 1, max_step)))
+        trial += 1
+        genes_mapped = []
+        best_mapped = []
 
-        pca = sklearn.decomposition.PCA(10,random_state=random_state)
-        principalComponents = pca.fit_transform(df.T)
-        principalDf = pandas.DataFrame(principalComponents)
-        principalDf.index = df.columns
+        pca = sklearn.decomposition.PCA(10, random_state=random_state)
+        principal_components = pca.fit_transform(df.T)
+        principal_df = pandas.DataFrame(principal_components)
+        principal_df.index = df.columns
 
         # explore PCs in parallel
-        pcs=numpy.arange(10)
-        tasks=[[df,principalDf,element,minNumberGenes] for element in pcs]
-        hydra=multiprocessing.pool.Pool(numCores)
-        genesMappedParallel=hydra.map(geneMapper,tasks)
-        for element in genesMappedParallel:
+        pcs = numpy.arange(10)
+        tasks = [(df, principal_df, element, min_number_genes) for element in pcs]
+        hydra = multiprocessing.pool.Pool(num_cores)
+        genes_mapped_parallel = hydra.map(gene_mapper, tasks)
+        for element in genes_mapped_parallel:
             for gene in element:
-                genesMapped.append(gene)
-        allGenesMapped.extend(genesMapped)
-        #print('genesMapped',len(genesMapped))
+                genes_mapped.append(gene)
+
+        all_genes_mapped.extend(genes_mapped)
+
         try:
-            stackGenes = numpy.hstack(genesMapped)
+            stackGenes = numpy.hstack(genes_mapped)
         except:
             stackGenes = []
-        residualGenes = list(set(df.index)-set(stackGenes))
-        df = df.loc[residualGenes,:]
+
+        residual_genes = list(set(df.index) - set(stackGenes))
+        df = df.loc[residual_genes, :]
 
         # significance surrogate in parallel
-        tasks=[[element,expressionData,expressionThreshold] for element in genesMapped]
-        parallelHits=hydra.map(parallelOverexpressSurrogate,tasks)
-        for parallelHit in parallelHits:
-            bestMapped.append(parallelHit[1])
-            if len(parallelHit[1]) > minNumberOverExpSamples:
-                bestHits.append(parallelHit[0])
-                    
-        if len(bestMapped)>0:            
-            countHits = Counter(numpy.hstack(bestMapped))
-            ranked = countHits.most_common()
-            dominant = [i[0] for i in ranked[0:int(numpy.ceil(0.1*len(ranked)))]]
+        tasks = [(element, expression_data, expression_threshold)
+                 for element in genes_mapped]
+        parallel_hits = hydra.map(parallel_overexpress_surrogate, tasks)
+
+        for element, hits in parallel_hits:
+            best_mapped.append(hits)
+            if len(hits) > min_number_overexp_samples:
+                best_hits.append(element)
+
+        if len(best_mapped) > 0:
+            count_hits = Counter(numpy.hstack(best_mapped))
+            ranked = count_hits.most_common()
+            dominant = [i[0] for i in ranked[0:int(numpy.ceil(0.1 * len(ranked)))]]
             remainder = [i for i in numpy.arange(df.shape[1]) if i not in dominant]
-            df = df.iloc[:,remainder]
+            df = df.iloc[:, remainder]
 
-    bestHits.sort(key=lambda s: -len(s))
-
-    return bestHits
+    best_hits.sort(key=lambda s: -len(s))
+    return best_hits
 
 def combineClusters(axes,clusters,threshold=0.925):
     combineAxes = {}
@@ -78,12 +85,12 @@ def combineClusters(axes,clusters,threshold=0.925):
         pearson = pearson_array(axesMatrix,axis)
         combine = numpy.where(pearson>threshold)[0]
         combineAxes[key] = filterKeys[combine]
-     
+
     revisedClusters = {}
     combinedKeys = decomposeDictionaryToLists(combineAxes)
     for keyList in combinedKeys:
         genes = list(set(numpy.hstack([clusters[i] for i in keyList])))
-        revisedClusters[len(revisedClusters)] = genes    
+        revisedClusters[len(revisedClusters)] = genes
 
     return revisedClusters
 
@@ -92,7 +99,7 @@ def decompose(geneset,expressionData,minNumberGenes=6):
     tst = numpy.multiply(fm,fm.T)
     tst[tst<numpy.percentile(tst,80)]=0
     tst[tst>0]=1
-    unmix_tst = unmix(tst)    
+    unmix_tst = unmix(tst)
     unmixedFiltered = [i for i in unmix_tst if len(i)>=minNumberGenes]
     return unmixedFiltered
 
@@ -149,36 +156,29 @@ def getAxes(clusters,expressionData):
         axes[key] = principalComponents.ravel()
     return axes
 
-def geneMapper(task):
+def gene_mapper(task):
+    genes_mapped = []
+    df, principal_df, i, min_number_genes = task
+    pearson = pearson_array(numpy.array(df), numpy.array(principal_df[i]))
+    highpass = max(numpy.percentile(pearson,95), 0.1)
+    lowpass = min(numpy.percentile(pearson,5), -0.1)
+    cluster1 = numpy.array(df.index[numpy.where(pearson > highpass)[0]])
+    cluster2 = numpy.array(df.index[numpy.where(pearson < lowpass)[0]])
 
-    genesMapped=[]
-    df=task[0]
-    principalDf=task[1]
-    i=task[2]
-    minNumberGenes=task[3]
-
-    pearson = pearson_array(numpy.array(df),numpy.array(principalDf[i]))
-    highpass = max(numpy.percentile(pearson,95),0.1)
-    lowpass = min(numpy.percentile(pearson,5),-0.1)
-    cluster1 = numpy.array(df.index[numpy.where(pearson>highpass)[0]])
-    cluster2 = numpy.array(df.index[numpy.where(pearson<lowpass)[0]])
-    
-    for clst in [cluster1,cluster2]:
-        pdc = recursiveAlignment(clst,expressionData=df,minNumberGenes=minNumberGenes)
-        if len(pdc)==0:
+    for clst in [cluster1, cluster2]:
+        pdc = recursiveAlignment(clst, expressionData=df, minNumberGenes=min_number_genes)
+        if len(pdc) == 0:
             continue
         elif len(pdc) == 1:
-            genesMapped.append(pdc[0])
+            genes_mapped.append(pdc[0])
         elif len(pdc) > 1:
             for j in range(len(pdc)-1):
-                if len(pdc[j]) > minNumberGenes:
-                    genesMapped.append(pdc[j])
+                if len(pdc[j]) > min_number_genes:
+                    genes_mapped.append(pdc[j])
 
-    #print(i,cluster1.shape,cluster2.shape,highpass,lowpass,len(genesMapped))
+    return genes_mapped
 
-    return genesMapped
-
-def iterativeCombination(dict_,key,iterations=25):    
+def iterativeCombination(dict_,key,iterations=25):
     initial = dict_[key]
     initialLength = len(initial)
     for iteration in range(iterations):
@@ -193,39 +193,38 @@ def iterativeCombination(dict_,key,iterations=25):
             initialLength = len(initial)
     return revised
 
-def parallelOverexpressSurrogate(task):
+def parallel_overexpress_surrogate(task):
+    element, expression_data, expression_threshold = task
 
-    element=task[0]
-    expressionData=task[1]
-    expressionThreshold=task[2]
-    
-    tmpCluster = expressionData.loc[element,:]
-    tmpCluster[tmpCluster<expressionThreshold] = 0
-    tmpCluster[tmpCluster>0] = 1
-    sumCluster = numpy.array(numpy.sum(tmpCluster,axis=0))
-    hits = numpy.where(sumCluster>0.333*len(element))[0]
+    tmp_cluster = expression_data.loc[element, :]
+    tmp_cluster[tmp_cluster < expression_threshold] = 0
+    tmp_cluster[tmp_cluster > 0] = 1
+    sum_cluster = numpy.array(numpy.sum(tmp_cluster, axis=0))
+    hits = numpy.where(sum_cluster > 0.333 * len(element))[0]
 
-    return [element,hits]
+    return (element, hits)
 
-def pearson_array(array,vector):    
+
+def pearson_array(array,vector):
     ybar = numpy.mean(vector)
-    sy = numpy.std(vector,ddof=1)    
+    sy = numpy.std(vector,ddof=1)
     yterms = (vector-ybar)/float(sy)
-    
+
     array_sx = numpy.std(array,axis=1,ddof=1)
-    
+
     if 0 in array_sx:
         passIndex = numpy.where(array_sx>0)[0]
         array = array[passIndex,:]
         array_sx = array_sx[passIndex]
-        
-    array_xbar = numpy.mean(array,axis=1)            
+
+    array_xbar = numpy.mean(array, axis=1)
     product_array = numpy.zeros(array.shape)
-    
+
     for i in range(0,product_array.shape[1]):
         product_array[:,i] = yterms[i]*(array[:,i] - array_xbar)/array_sx
-        
+
     return numpy.sum(product_array,axis=1)/float(product_array.shape[1]-1)
+
 
 def processCoexpressionLists(lists,expressionData,threshold=0.925):
     reconstructed = reconstruction(lists,expressionData,threshold)
