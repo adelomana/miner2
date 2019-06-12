93930e716af36b288cbdcfb001a6b31f182f91d6
Wei-ju Wu <weiju.wu@gmail.com>
2019-06-12 12:46:57 -0700

    added a test set for the cluster function

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 PYTHONPATH=. python3 test/preprocess_test.py
+PYTHONPATH=. python3 test/coexpression_test.py
--- run_test_python2.sh
+++ run_test_python2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 PYTHONPATH=. python test/preprocess_test.py
+PYTHONPATH=. python test/coexpression_test.py
