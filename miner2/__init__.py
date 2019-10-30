0d6d66fd73dac554411008cf344fd6798d225513
Wei-ju Wu <weiju.wu@gmail.com>
2019-10-30 12:31:56 -0700

    write depenency information into each run output

--- miner2/__init__.py
+++ miner2/__init__.py
@@ -9,4 +9,4 @@ except:
     __version__ = 'development'
 
 message=str(__version__).replace('miner2','miner2 version')
-print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S \t hello from {}".format(message)))
+#print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S \t hello from {}".format(message)))
--- miner2/util.py
+++ miner2/util.py
@@ -1,5 +1,21 @@
-import numpy
+import numpy, scipy, pandas, sklearn, lifelines, matplotlib, seaborn
+
 import multiprocessing, multiprocessing.pool
+import sys
+
+
+def write_dependency_infos(outfile):
+    py_version = map(str, list(sys.version_info[0:3]))
+    outfile.write('Python %s\n\n' % ('.'.join(py_version)))
+    outfile.write('Dependencies:\n')
+    outfile.write('-------------\n\n')
+    outfile.write('numpy: %s\n' % numpy.__version__)
+    outfile.write('scipy: %s\n' % scipy.__version__)
+    outfile.write('pandas: %s\n' % scipy.__version__)
+    outfile.write('sklearn: %s\n' % sklearn.__version__)
+    outfile.write('lifelines: %s\n' % lifelines.__version__)
+    outfile.write('matplotlib: %s\n' % matplotlib.__version__)
+    outfile.write('seaborn: %s\n' % seaborn.__version__)
 
 
 def split_for_multiprocessing(vector, cores):
